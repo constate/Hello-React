3f2411d02b1f1f4b8e3f4708c74a28336248c641
sunghyun Kim <constate@naver.com>
2021-11-20 17:36:06 +0900

    2021.11.20

---
+++ src/MyComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+
+class MyComponent extends Component {
+  static defaultProps = {
+    name : '기본 이름'
+  };
+
+  static propTypes = {
+    name: propTypes.string,
+    favoriteNumber : propTypes.number.isRequired
+  };
+  
+  render() {
+    const {name, favoriteNumber, children} = this.props;
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다. <br />
+        children 값은 {children}입니다. <br />
+        제가 좋아하는 숫자는 {favoriteNumber}입니다.
+      </div>
+    );
+  }
+}
+
+// MyComponent.defaultProps ={
+//   name : '기본 이름'
+// };
+
+// MyComponent.propTypes ={
+//   name : propTypes.string,
+//   favoriteNumber: propTypes.number.isRequired
+// }
+
+export default MyComponent;
